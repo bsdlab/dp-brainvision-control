c9224bf5e4987b61a4c185899b2fdef823ce1e67
matthiasdold <matthias.dold@gmx.net>
2025-04-23 20:31:32 +0200

    auto format

--- api/server.py
+++ api/server.py
@@ -1,17 +1,17 @@
+from dareplane_utils.default_server.server import DefaultServer
 from fire import Fire
 
 from brainvision_recording.main import init_bvm
 from brainvision_recording.utils.logging import logger
-from dareplane_utils.default_server.server import DefaultServer
 
 
 def main(port: int = 8080, ip: str = "127.0.0.1", loglevel: int = 10):
     logger.setLevel(loglevel)
     bvm = init_bvm()
     pcommand_map = {
-        'SET_SAVE_PATH': bvm.set_rec_dir,
-        'START_SAVE': bvm.start_recording,
-        'STOP_SAVE': bvm.stop_recording,
+        "SET_SAVE_PATH": bvm.set_rec_dir,
+        "START_SAVE": bvm.start_recording,
+        "STOP_SAVE": bvm.stop_recording,
     }
 
     server = DefaultServer(
@@ -24,6 +24,7 @@ def main(port: int = 8080, ip: str = "127.0.0.1", loglevel: int = 10):
     server.start_listening()
 
     return 0
+
 
 if __name__ == "__main__":
     Fire(main)
