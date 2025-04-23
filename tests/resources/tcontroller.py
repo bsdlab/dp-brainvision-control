97e376e9f6d1847d7290915f0b4e54f1585169be
matthiasdold <matthias.dold@gmx.net>
2025-04-23 20:37:18 +0200

    more description for test

--- tests/resources/tcontroller.py
+++ tests/resources/tcontroller.py
@@ -1,31 +1,30 @@
 import time
-from brainvision_recording.utils.logging import logger
+from pathlib import Path
+
 from brainvision_recording.controller import BrainvisionManager
+from brainvision_recording.utils.logging import logger
 
 
 class DummyManager(BrainvisionManager):
 
-    def _run_vbs_cmd(self, script_name: str, params: None | list = []):
+    def _run_vbs_cmd(self, script_name: str, params: None | list = []) -> Path:
         script_path = self.vbs_root_path.joinpath(script_name)
-        logger.debug(f'vbs script called at: {script_path}')
+        logger.debug(f"vbs script called at: {script_path}")
         return script_path
 
-    def start_recording(self, rec_name: str,
-                        overwrite: bool = False):
+    def start_recording(self, rec_name: str, overwrite: bool = False):
 
         self.rec_dir.mkdir(parents=True, exist_ok=True)
 
-        fname = self.rec_dir.joinpath(f'{rec_name}.eeg')
+        fname = self.rec_dir.joinpath(f"{rec_name}.eeg")
         if fname.exists() and not overwrite:
             # find latest suffix and add next
             fname = fname.parent.joinpath(
-                fname.stem + '_' + time.strftime('%Y%m%d%H%M%S')
-                + fname.suffix)
+                fname.stem + "_" + time.strftime("%Y%m%d%H%M%S") + fname.suffix
+            )
 
         # Dummy write here
-        with open(fname, 'w') as fh:
-            fh.write('>> ' + time.strftime('%Y%m%d%H%M%S'))
+        with open(fname, "w") as fh:
+            fh.write(">> " + time.strftime("%Y%m%d%H%M%S"))
 
-        return self._run_vbs_cmd(
-            'bvr_startrecording.vbs',
-            [str(fname)])
+        return self._run_vbs_cmd("bvr_startrecording.vbs", [str(fname)])
--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,40 +1,36 @@
-import pytest
-
 from pathlib import Path
+
+import pytest
 
 from tests.resources.tcontroller import DummyManager
 
+# Note: The tests do only check against a DummyManager, which only validates
+#       that commands are sent to the correct path, not actually triggering the
+#       vbs scripts. This is done so that the module can also be developed under
+#       UNIX.
+
 
 def test_start_recording(tmp_path):
-    bvm = DummyManager(rec_dir=tmp_path,
-                       vbs_root_path=Path('./vsbs/'))
+    bvm = DummyManager(rec_dir=tmp_path, vbs_root_path=Path("./vsbs/"))
 
-    bvm.start_recording('test_block_1')
-    eeg_files = list(tmp_path.glob('*.eeg'))
+    bvm.start_recording("test_block_1")
+    eeg_files = list(tmp_path.glob("*.eeg"))
     assert len(eeg_files) == 1
 
     # no overwrite, so a new file should be created
-    bvm.start_recording('test_block_1')
-    eeg_files2 = list(tmp_path.glob('*.eeg'))
+    bvm.start_recording("test_block_1")
+    eeg_files2 = list(tmp_path.glob("*.eeg"))
     assert len(eeg_files2) == 2
 
     # with overwrite, so no new file should be created
-    bvm.start_recording('test_block_1', overwrite=True)
-    eeg_files3 = list(tmp_path.glob('*.eeg'))
+    bvm.start_recording("test_block_1", overwrite=True)
+    eeg_files3 = list(tmp_path.glob("*.eeg"))
     assert len(eeg_files3) == 2
 
 
 def test_other_vbs_calls(tmp_path):
-    bvm = DummyManager(
-        rec_dir=tmp_path,
-        vbs_root_path=Path('./vsbs/'))
+    bvm = DummyManager(rec_dir=tmp_path, vbs_root_path=Path("./vsbs/"))
     ret = bvm.stop_recording()
-    assert ret.stem.endswith('bvr_stoprecording')
+    assert ret.stem.endswith("bvr_stoprecording")
     ret = bvm.view_signals()
-    assert ret.stem.endswith('bvr_viewsignals')
-
-
-
-
-
-
+    assert ret.stem.endswith("bvr_viewsignals")
